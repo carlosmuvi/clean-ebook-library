4488c386d0e77be5d20496d9d3b786e760aaa203
Carlos Muñoz <kalospiles@gmail.com>
2015-08-08 16:18:40 +0200

    added default subscriber

---
+++ domain/src/main/java/carlosmuvi/bqsample/interactors/DefaultSubscriber.java
@@ -0,0 +1,18 @@
+package carlosmuvi.bqsample.interactors;
+
+/**
+ * Created by carlos.
+ */
+public class DefaultSubscriber<T> extends rx.Subscriber<T> {
+  @Override public void onCompleted() {
+    // no-op by default.
+  }
+
+  @Override public void onError(Throwable e) {
+    // no-op by default.
+  }
+
+  @Override public void onNext(T t) {
+    // no-op by default.
+  }
+}
